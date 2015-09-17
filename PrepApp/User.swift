92d693c3e29667d0bbec7a77d11c6c53c1236b44
Mikael Vandeginste <MIKALA@iMac-de-Mikael.local>
2015-09-17 20:05:29 +0200

    few fixes (separator)

--- PrepApp/User.swift
+++ PrepApp/User.swift
@@ -152,7 +152,7 @@ class User {
             failed: data["failed"] as! Int,
 			success: data["success"] as! Int,
             weeksBeforeExam: 7,
-            awardPointsApp: data["award_points"] as! Int,,
+            awardPointsApp: data["award_points"] as! Int,
             awardPointsGlobal: 0
         )
         
